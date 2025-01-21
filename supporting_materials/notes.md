7ba754919afc223c7cc2c79b4f889af63f24ff0b
aleph <124294237+philipgalebach@users.noreply.github.com>
2025-01-21 16:49:39 -0300

    updated readme

---
+++ supporting_materials/notes.md
@@ -0,0 +1,1 @@
+app url is at https://readiverse-haven.vercel.app/
--- supporting_materials/supabase_policies.sql
+++ supporting_materials/supabase_policies.sql
@@ -5,3 +5,8 @@ TO public
 USING (
   bucket_id = (SELECT id FROM storage.buckets WHERE name = 'epub_files')
 );
+
+
+ALTER TABLE books ENABLE ROW LEVEL SECURITY;
+REVOKE ALL ON ALL TABLES IN SCHEMA public FROM public;
+REVOKE USAGE ON SCHEMA public FROM anon;
