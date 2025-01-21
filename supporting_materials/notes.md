a90aed8daca95604d13958d3c015c014fa4c6271
aleph <124294237+philipgalebach@users.noreply.github.com>
2025-01-21 16:49:39 -0300

    updated readme

--- supporting_materials/notes.md
+++ supporting_materials/notes.md
@@ -1,7 +1,10 @@
 app url is at https://readiverse-haven.vercel.app/
+<<<<<<< HEAD
 
 
 the app is working at 
 
 https://readiverse-haven-git-v1hosted-alex-jakubowskis-projects.vercel.app/
 https://readiverse-haven-git-v1hosted-alex-jakubowskis-projects.vercel.app/pride-and-prejudice
+=======
+>>>>>>> 7ba7549 (updated readme)
--- supporting_materials/supabase_policies.sql
+++ supporting_materials/supabase_policies.sql
@@ -6,6 +6,7 @@ USING (
   bucket_id = (SELECT id FROM storage.buckets WHERE name = 'epub_files')
 );
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 -- a. Revoke existing privileges from the public role:
 ALTER TABLE books ENABLE ROW LEVEL SECURITY;
@@ -13,3 +14,10 @@ REVOKE ALL ON ALL TABLES IN SCHEMA public FROM public;
 REVOKE USAGE ON SCHEMA public FROM anon;
 =======
 >>>>>>> 756861c (commiting notes)
+=======
+
+
+ALTER TABLE books ENABLE ROW LEVEL SECURITY;
+REVOKE ALL ON ALL TABLES IN SCHEMA public FROM public;
+REVOKE USAGE ON SCHEMA public FROM anon;
+>>>>>>> 7ba7549 (updated readme)
